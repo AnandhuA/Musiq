c5188d9c0dabc9987ffbf71727f9350ffaf128c7
AnandhuA <anandhu1407@gmail.com>
2024-10-21 15:36:56 +0530

    api url page added and add search funtions

---
+++ lib/core/api_urls.dart
@@ -0,0 +1,15 @@
+class ApiUrls {
+
+
+  static const String homeData = "https://jiosaavn-api-rs.vercel.app/modules";
+  static const String album = "https://jiosaavn-api-rs.vercel.app/album?id=";
+  static const String playlist = "https://saavn.dev/api/playlists?id=";
+  static const String globalSearch = "https://saavn.dev/api/search?query=";
+  static const String searchSong = "https://saavn.dev/api/search/songs?query=";
+  static const String searchAlbums =
+      "https://saavn.dev/api/search/albums?query=";
+  static const String searchArtists =
+      "https://saavn.dev/api/search/artists?query=";
+  static const String searchPlayList =
+      "https://saavn.dev/api/search/playlists?query=";
+}
--- lib/data/savan_2.0.dart
+++ lib/data/savan_2.0.dart
@@ -1,16 +1,10 @@
-
 import 'package:http/http.dart' as http;
+import 'package:musiq/core/api_urls.dart';
 
 class Saavan2 {
-  static const String baseurl = "https://jiosaavn-api-rs.vercel.app/modules";
-  static const String album = "https://jiosaavn-api-rs.vercel.app/album?id=";
-  static const String playlist = "https://saavn.dev/api/playlists?id=";
-
   static Future<http.Response?> featchHomeScreenModel() async {
     try {
-      final responce = await http.get(Uri.parse(baseurl));
-      // log("status code${responce.statusCode}");
-      // log("${responce.body}");
+      final responce = await http.get(Uri.parse(ApiUrls.homeData));
       return responce;
     } catch (e) {
       return null;
@@ -19,10 +13,7 @@ class Saavan2 {
 
   static Future<http.Response?> featchAlbum({required String albumId}) async {
     try {
-      final responce = await http.get(Uri.parse("$album$albumId"));
-      // log("status code${responce.statusCode}");
-      // log("${responce.body}");
-
+      final responce = await http.get(Uri.parse("${ApiUrls.album}$albumId"));
       return responce;
     } catch (e) {
       return null;
@@ -32,11 +23,63 @@ class Saavan2 {
   static Future<http.Response?> featchPlayList(
       {required String playlistId}) async {
     try {
-      final responce = await http.get(Uri.parse("$playlist$playlistId"));
-
-      // log("status code${responce.statusCode}");
-      // log("${responce.body}");
+      final responce =
+          await http.get(Uri.parse("${ApiUrls.playlist}$playlistId"));
       return responce;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<http.Response?> featchglobalSearch(
+      {required String query}) async {
+    try {
+      final response =
+          await http.get(Uri.parse("${ApiUrls.globalSearch}$query"));
+      return response;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<http.Response?> featchSearchSong(
+      {required String query}) async {
+    try {
+      final response = await http.get(Uri.parse("${ApiUrls.searchSong}$query"));
+      return response;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<http.Response?> featchSearchAlbums(
+      {required String query}) async {
+    try {
+      final response =
+          await http.get(Uri.parse("${ApiUrls.searchAlbums}$query"));
+      return response;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<http.Response?> featchSearchArtists(
+      {required String query}) async {
+    try {
+      final response =
+          await http.get(Uri.parse("${ApiUrls.searchArtists}$query"));
+      return response;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<http.Response?> featchSearchPlayList(
+      {required String query}) async {
+    try {
+      final response =
+          await http.get(Uri.parse("${ApiUrls.searchPlayList}$query"));
+      return response;
     } catch (e) {
       return null;
     }
