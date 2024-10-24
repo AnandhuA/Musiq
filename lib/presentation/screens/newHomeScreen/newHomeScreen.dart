21045d94375f078d09b5725578daeef2e18ef74f
AnandhuA <anandhu1407@gmail.com>
2024-10-24 16:25:55 +0530

    add new home page data

--- lib/presentation/screens/newHomeScreen/newHomeScreen.dart
+++ lib/presentation/screens/newHomeScreen/newHomeScreen.dart
@@ -6,7 +6,7 @@ import 'package:musiq/bloc/home_screen_cubit/home_screen_cubit.dart';
 import 'package:musiq/core/colors.dart';
 import 'package:musiq/core/global_variables.dart';
 import 'package:musiq/core/helper_funtions.dart';
-import 'package:musiq/data/savan_2.0.dart';
+import 'package:musiq/presentation/commanWidgets/empty_screen.dart';
 import 'package:musiq/presentation/screens/album_or_playlist_screen/new_album_or_playlist_screen.dart';
 
 class Newhomescreen extends StatelessWidget {
@@ -18,7 +18,7 @@ class Newhomescreen extends StatelessWidget {
       body: BlocBuilder<HomeScreenCubit, HomeScreenState>(
         builder: (context, state) {
           if (state is HomeScreenLoading) {
-            Center(
+            return Center(
               child: CircularProgressIndicator(
                 color: AppColors.colorList[AppGlobals().colorIndex],
               ),
@@ -43,7 +43,7 @@ class Newhomescreen extends StatelessWidget {
                     },
                   );
                 } else if (state is FeatchAlbumAndPlayListLoaded) {
-                  Navigator.pop(context); // for close loading
+                  Navigator.pop(context); // for closing loading
                   Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -58,116 +58,173 @@ class Newhomescreen extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    if (state.newHomeScreenModel?.songdata?.trending?.data !=
-                        null)
-                      Container(
-                        height: 200,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: state.newHomeScreenModel!.songdata!
-                              .trending!.data!.length,
-                          itemBuilder: (context, index) {
-                            final data = state.newHomeScreenModel!.songdata!
-                                .trending!.data![index];
-
-                            return GestureDetector(
-                              onTap: () {
-                                context
-                                    .read<FeatchAlbumAndPlayListCubit>()
-                                    .fetchData(
-                                        type: data.type ?? "",
-                                        id: data.id ?? "0",
-                                        imageUrl: data.image!.last.imageUrl ??
-                                            errorImage());
-                              },
-                              child: Container(
-                                margin: EdgeInsets.all(10),
-                                height: 200,
-                                width: 150,
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: CachedNetworkImage(
-                                          imageUrl:
-                                              data.image!.last.imageUrl ?? "",
-                                          errorWidget: (context, url, error) =>
-                                              Image.asset(
-                                                  "assets/images/album.png"),
-                                          placeholder: (context, url) =>
-                                              Image.asset(
-                                                  "assets/images/album.png"),
-                                        ),
-                                      ),
-                                    ),
-                                    Text(
-                                      data.name ?? "null",
-                                      maxLines: 1,
-                                    ),
-                                    Text(
-                                      data.type ?? "null",
-                                      maxLines: 1,
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    Container(
-                      height: 200,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: state
-                            .newHomeScreenModel!.songdata!.mixes!.data!.length,
-                        itemBuilder: (context, index) {
-                          final data = state.newHomeScreenModel!.songdata!
-                              .mixes!.data![index];
-                          return GestureDetector(
-                            onTap: () {
-                              Saavan2.featchAlbum(albumId: data.id ?? "0");
-                            },
-                            child: Container(
-                              margin: EdgeInsets.all(10),
-                              height: 200,
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    height: 150,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: CachedNetworkImage(
-                                        imageUrl:
-                                            data.image!.last.imageUrl ?? "",
-                                        errorWidget: (context, url, error) =>
-                                            Image.asset(
-                                                "assets/images/album.png"),
-                                        placeholder: (context, url) =>
-                                            Image.asset(
-                                                "assets/images/album.png"),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(data.name ?? "null")
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    )
+                    _buildHorizontalListView(
+                        context,
+                        "Trending",
+                        state.newHomeScreenModel?.songdata?.trending?.data ??
+                            []),
+                    _buildHorizontalListView(context, "Mixes",
+                        state.newHomeScreenModel?.songdata?.mixes?.data ?? []),
+                    _buildHorizontalListView(
+                        context,
+                        "Playlists",
+                        state.newHomeScreenModel?.songdata?.playlists?.data ??
+                            []),
+                    _buildHorizontalListView(context, "Albums",
+                        state.newHomeScreenModel?.songdata?.albums?.data ?? []),
+                    _buildHorizontalListView(
+                        context,
+                        "ArtistRecos",
+                        state.newHomeScreenModel?.songdata?.artistRecos?.data ??
+                            []),
+                    _buildHorizontalListView(context, "Charts",
+                        state.newHomeScreenModel?.songdata?.charts?.data ?? []),
+                    _buildHorizontalListView(
+                        context,
+                        "CityMod",
+                        state.newHomeScreenModel?.songdata?.cityMod?.data ??
+                            []),
+                    _buildHorizontalListView(
+                        context,
+                        "Discover",
+                        state.newHomeScreenModel?.songdata?.discover?.data ??
+                            []),
+                    _buildHorizontalListView(context, "Radio",
+                        state.newHomeScreenModel?.songdata?.radio?.data ?? []),
+                    _buildHorizontalListView(context, "Promo0",
+                        state.newHomeScreenModel?.songdata?.promo0?.data ?? []),
+                    _buildHorizontalListView(context, "Promo1",
+                        state.newHomeScreenModel?.songdata?.promo1?.data ?? []),
+                    _buildHorizontalListView(context, "Promo2",
+                        state.newHomeScreenModel?.songdata?.promo2?.data ?? []),
+                    _buildHorizontalListView(context, "Promo3",
+                        state.newHomeScreenModel?.songdata?.promo3?.data ?? []),
+                    _buildHorizontalListView(context, "Promo4",
+                        state.newHomeScreenModel?.songdata?.promo4?.data ?? []),
+                    _buildHorizontalListView(context, "Promo5",
+                        state.newHomeScreenModel?.songdata?.promo5?.data ?? []),
+                    _buildHorizontalListView(context, "Promo6",
+                        state.newHomeScreenModel?.songdata?.promo6?.data ?? []),
+                    _buildHorizontalListView(context, "Promo7",
+                        state.newHomeScreenModel?.songdata?.promo7?.data ?? []),
+                    _buildHorizontalListView(context, "Promo8",
+                        state.newHomeScreenModel?.songdata?.promo8?.data ?? []),
+                    _buildHorizontalListView(context, "Promo9",
+                        state.newHomeScreenModel?.songdata?.promo9?.data ?? []),
+                    _buildHorizontalListView(
+                        context,
+                        "Promo10",
+                        state.newHomeScreenModel?.songdata?.promo10?.data ??
+                            []),
+                    _buildHorizontalListView(
+                        context,
+                        "Promo11",
+                        state.newHomeScreenModel?.songdata?.promo11?.data ??
+                            []),
+                    _buildHorizontalListView(
+                        context,
+                        "Promo12",
+                        state.newHomeScreenModel?.songdata?.promo12?.data ??
+                            []),
+                    _buildHorizontalListView(
+                        context,
+                        "Promo13",
+                        state.newHomeScreenModel?.songdata?.promo13?.data ??
+                            []),
                   ],
                 ),
               ),
             );
           }
-          return Center(
-            child: Text("Error"),
+          return emptyScreen(
+            context: context,
+            text1: "show",
+            size1: 15,
+            text2: "Nothing",
+            size2: 20,
+            text3: "result",
+            size3: 20,
           );
         },
       ),
+    );
+  }
+
+  Widget _buildHorizontalListView(
+      BuildContext context, String sectionTitle, List<dynamic> dataList) {
+    if (dataList.isEmpty) {
+      return SizedBox.shrink();
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            sectionTitle,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.colorList[AppGlobals().colorIndex],
+            ),
+          ),
+        ),
+        Container(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: dataList.length,
+            itemBuilder: (context, index) {
+              final data = dataList[index];
+
+              return GestureDetector(
+                onTap: () {
+                  context.read<FeatchAlbumAndPlayListCubit>().fetchData(
+                      type: data.type ?? "",
+                      id: data.id ?? "0",
+                      imageUrl: data.image?.last.imageUrl ?? errorImage());
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 150,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            imageUrl: data.image?.last.imageUrl ?? "",
+                            errorWidget: (context, url, error) =>
+                                Image.asset("assets/images/album.png"),
+                            placeholder: (context, url) =>
+                                Image.asset("assets/images/album.png"),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        data.name ?? "null",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text(
+                        data.type ?? "null",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey.withOpacity(0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
