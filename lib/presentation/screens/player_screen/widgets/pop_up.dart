1e5f3c2a27c6212986bdabcd1d8e63c565c24739
AnandhuA <anandhu1407@gmail.com>
2024-10-28 20:43:28 +0530

    bug fix

--- lib/presentation/screens/player_screen/widgets/pop_up.dart
+++ lib/presentation/screens/player_screen/widgets/pop_up.dart
@@ -17,62 +17,60 @@ Future<Widget?> popUpWiget({
       borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
     ),
     builder: (BuildContext context) {
-      return Expanded(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: GridView.builder(
-            itemCount: list.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              childAspectRatio: 0.8,
-            ),
-            itemBuilder: (context, index) {
-              String? imageUrl;
-
-              if (list[index].image != null && list[index].image!.isNotEmpty) {
-                imageUrl = list[index].image!.last.imageUrl;
-              } else {
-                log("Image list is null or empty for index $index. Using placeholder image.");
-                imageUrl = errorImage();
-              }
-
-              if (!isValidUrl(imageUrl)) {
-                log("Invalid image URL: $imageUrl");
-                imageUrl = errorImage();
-              }
-
-              return GestureDetector(
-                onTap: () {
-                  log("Image URL: $imageUrl");
-                  context.read<FeatchSongCubit>().featchArtistSongs(
-                      id: list[index].id ?? "0",
-                      imageUrl: imageUrl ?? errorImage());
-                },
-                child: Container(
-                  child: Column(
-                    children: [
-                      if (list[index].image != null &&
-                          list[index].image!.isNotEmpty)
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: CachedNetworkImage(imageUrl: imageUrl),
-                        )
-                      else
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: albumImagePlaceholder()),
-                      Text(
-                        list[index].name ?? "No",
-                        maxLines: 1,
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
+      return Padding(
+        padding: const EdgeInsets.all(12),
+        child: GridView.builder(
+          itemCount: list.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 0.8,
           ),
+          itemBuilder: (context, index) {
+            String? imageUrl;
+
+            if (list[index].image != null && list[index].image!.isNotEmpty) {
+              imageUrl = list[index].image!.last.imageUrl;
+            } else {
+              log("Image list is null or empty for index $index. Using placeholder image.");
+              imageUrl = errorImage();
+            }
+
+            if (!isValidUrl(imageUrl)) {
+              log("Invalid image URL: $imageUrl");
+              imageUrl = errorImage();
+            }
+
+            return GestureDetector(
+              onTap: () {
+                log("Image URL: $imageUrl");
+                context.read<FeatchSongCubit>().featchArtistSongs(
+                    id: list[index].id ?? "0",
+                    imageUrl: imageUrl ?? errorImage());
+              },
+              child: Container(
+                child: Column(
+                  children: [
+                    if (list[index].image != null &&
+                        list[index].image!.isNotEmpty)
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: CachedNetworkImage(imageUrl: imageUrl),
+                      )
+                    else
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: albumImagePlaceholder()),
+                    Text(
+                      list[index].name ?? "No",
+                      maxLines: 1,
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       );
     },
